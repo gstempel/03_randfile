8b90096b23d9f1991fb4214dacea7da887b44941
Grace Stempel <gracestempel@Katara.home>
2016-10-26 17:46:47 -0400

    continue stage 2

--- randfile.c
+++ randfile.c
@@ -8,7 +8,7 @@ unsigned ranGen() { //get rand nums from /dev/random
   int data = open("/dev/random", O_RDONLY);
   unsigned ret;
   read(data, &ret, 4);
-  printf("%u\n", ret);
+  //printf("%u\n", ret);
   close(data);
   return ret;
 }
@@ -26,9 +26,16 @@ int main() {
   }
 
   //STEP 2: WRITE THIS ARRAY OF RAND NUMS TO FILE
+  printf("Writing numbers to file...\n");
   int intermed = open("intermed.txt",O_WRONLY | O_CREAT, 0666);
-  printf("foo[0]: %d\n",foo[0]);
-  write(intermed, foo[0], 4); //okay this is writin but how write actual num?
+  //printf("foo[0]: %d\n",foo[0]);
+  write(intermed, foo, 4*size); //okay this is writin but how write actual num?
+  unsigned* boo[size];
+  printf("Reading numbers from file...\n");
+  read(intermed, boo, 4*size);
+  for(i=0; i < size; i++) {
+    printf("random %d: %u\n", i, boo[i]);
+  }
 
   //STEP 3:
   return 0;
