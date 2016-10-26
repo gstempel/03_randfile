ea874e10b9ac5e07957b2323bd24d2836f7f48c9
eVookelj <evukelj@stuy.edu>
2016-10-26 15:34:58 -0400

    Start part 2

--- randfile.c
+++ randfile.c
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int ranGen() {
+unsigned* ranGen() {
   int data = open("/dev/random", O_RDONLY);
   printf("opening works\n");
   unsigned *ret;
@@ -24,6 +24,7 @@ int main() {
     printf("\nGetting for loop\n");
     printf("random %d: %d\n", i, foo[i]);
   }
-
+  int intermed = open("intermed.txt",O_RDWR | O_CREAT, 0644);
+  write(intermed, foo[0], 4);
   return 0;
 }
