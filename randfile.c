a17dc9190e7265d652e676caec12b706a351eaa1
Grace Stempel <gracestempel@Katara.home>
2016-10-26 22:54:51 -0400

    finish

--- randfile.c
+++ randfile.c
@@ -3,13 +3,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
 unsigned ranGen() { //get rand nums from /dev/random
   int data = open("/dev/random", O_RDONLY);
+  if(data == -1) {printf("%s\n",strerror(errno));}
+
   unsigned ret;
-  read(data, &ret, 4);
+  int r;
+  r = read(data, &ret, 4);
+  if(r == -1) {printf("%s\n",strerror(errno));}
+
   //printf("%u\n", ret);
-  close(data);
+  r = close(data);
+  if(r == -1) {printf("%s\n",strerror(errno));}
   return ret;
 }
 
@@ -28,12 +36,23 @@ int main() {
   //STEP 2: WRITE THIS ARRAY OF RAND NUMS TO FILE
   printf("\nWriting numbers to file...\n");
   int intermed = open("intermed.txt",O_WRONLY | O_CREAT, 0666);
-  write(intermed, foo, 4*size); //okay this is writin but how write actual num?
+  if(intermed == -1) {printf("%s\n",strerror(errno));}
+
+  int w;
+  w = write(intermed, foo, 4*size);
+  if(w == -1) {printf("%s\n",strerror(errno));}
+
   intermed = close(intermed);
+  if(intermed == -1) {printf("%s\n",strerror(errno));}
+
   intermed = open("intermed.txt", O_RDONLY, 0666);
+  if(intermed == -1) {printf("%s\n",strerror(errno));}
+
   unsigned boo[size];
   printf("\nReading numbers from file...\nVerification: \n");
-  read(intermed, boo, 4*size);
+  w = read(intermed, boo, 4*size);
+  if(w == -1) {printf("%s\n",strerror(errno));}
+
   for(i=0; i < size; i++) {
     printf("random %d: %u\n", i, boo[i]);
   }
